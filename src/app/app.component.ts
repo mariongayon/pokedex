345908897c2a280d1fee02ff0df112073775ca67
Marion Gayon <mariongayon@gmail.com>
2022-07-28 09:58:13 +0200

    add the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-pokemon-app';
+export class AppComponent implements OnInit {
+
+  //private pokemons: Pokemon[];
+  pokemons = POKEMONS;
+  color: string = '';
+
+  ngOnInit(): void {
+    this.pokemons = POKEMONS;
+    console.log(this.pokemons);
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log('Le pokemon sélectionné est:' + pokemon.name )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColor } from './pokemon-type-color';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BorderCardDirective,
+    PokemonTypeColor
   ],
   imports: [
     BrowserModule
